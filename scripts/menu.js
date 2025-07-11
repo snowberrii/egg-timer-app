15025f00afcdb67a8ad3b0dfdb24ea8a992d804d
Snow <termwan.c@gmail.com>
2025-07-10 22:34:30 -0700

    add direction to countdown page

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,3 +4,19 @@ function instructionPage() {
 
 const goBackBtn = document.querySelector(".back");
 goBackBtn.addEventListener("click", instructionPage);
+
+
+function countdownPage() {
+  window.location.href = "timer.html";
+}
+
+const runnyYolk = document.querySelector("#runny-yolk");
+const softBoiled = document.querySelector("#soft-boiled");
+const mediumBoiled = document.querySelector("#medium-boiled");
+const hardBoiled = document.querySelector("#hard-boiled");
+
+
+runnyYolk.addEventListener("click", countdownPage);
+softBoiled.addEventListener("click", countdownPage);
+mediumBoiled.addEventListener("click", countdownPage);
+hardBoiled.addEventListener("click", countdownPage);
